5f71a222897a65eb0bcb874582a89776b21d78b9
FIRST_Zeenat LAST_Azeem <Shadbkhat13@gmail.com>
2023-04-07 22:28:15 +0500

    first commit

---
+++ script/script3.js
@@ -0,0 +1,32 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>JavaScript</title>
+// <link rel="stylesheet" href="./style.css/html">
+// Day 2
+
+// <
+   
+
+// </script>
+// <script type="text/javascript">
+//     alert('Welcome to JavaScript');
+// <script type="text/javascript">
+// alert(5+4);
+// </script>
+// <script type="text/javascript">
+//     document.write('Welcome to JavaScript')
+// </script>
+
+// </head>
+// <body>
+    
+// </body>
+// </html>
+var zakatperstange=0.025;
+var user_input=Numberprompt("Enter a number");
+var result=(zakatpecentage)*(userinput);
+alert("your zakat value is"+ result);
